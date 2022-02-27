19c82d33a3efe11bd6c78a24561b04387c9170b4
ServerJon <ctsesiphon@hotmail.com>
2022-02-27 09:06:36 +0100

    initial commit

---
+++ apps/albums-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'curso-angular-multiplataforma-open-webinars-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'albums-web';
+}
