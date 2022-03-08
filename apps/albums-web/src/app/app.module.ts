36f6a4363f32021d6ba9c9d2d26f3142879e4735
ServerJon <ctsesiphon@hotmail.com>
2022-03-08 07:31:23 +0100

    instalacion pwa

--- apps/albums-web/src/app/app.module.ts
+++ apps/albums-web/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production,
+            // Register the ServiceWorker as soon as the app is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000',
+        }),
+    ],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
