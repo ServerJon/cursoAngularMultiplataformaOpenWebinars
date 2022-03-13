8cc4acf2207a13650fb85f5a44816db0113ca83e
ServerJon <ctsesiphon@hotmail.com>
2022-03-13 10:49:40 +0100

    instalacion y configuracion ionic

---
+++ apps/albums-mobile/src/app/folder/folder.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    selector: 'curso-angular-multiplataforma-open-webinars-folder',
+    templateUrl: './folder.page.html',
+    styleUrls: ['./folder.page.scss'],
+})
+export class FolderPage implements OnInit {
+    public folder!: string;
+
+    constructor(private activatedRoute: ActivatedRoute) {}
+
+    ngOnInit() {
+        const id = this.activatedRoute.snapshot.paramMap.get('id');
+
+        if (id) {
+            this.folder = id;
+        }
+    }
+}
